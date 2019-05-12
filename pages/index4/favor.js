11cafb1cf5432f0a44e8c90dc25d75fa4cdabdb9
Haicang <aseaboat@outlook.com>
2019-05-12 22:35:40 +0800

    change name

--- pages/index4/favor.js
+++ pages/index4/favor.js
@@ -1,4 +1,10 @@
 // pages/index4/favor.js
+
+var time = 0;
+var touchDot = 0;//触摸时的原点
+var interval = "";
+var flag_hd = true;
+
 Page({
 
   /**
@@ -59,7 +65,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
   },
 
   /**
@@ -73,7 +79,44 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    flag_hd = true;    //重新进入页面之后，可以再次执行滑动切换页面代码
+    clearInterval(interval); // 清除setInterval
+    time = 0;
+  },
 
+  // 触摸开始事件
+  touchStart: function (e) {
+    touchDot = e.touches[0].pageX; // 获取触摸时的原点
+    // 使用js计时器记录时间    
+    interval = setInterval(function () {
+      time++;
+    }, 100);
+  },
+  // 触摸结束事件
+  touchEnd: function (e) {
+    var touchMove = e.changedTouches[0].pageX;
+    // 向左滑动   
+    if (touchMove - touchDot <= -40 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      this.TabCur = this.TabCur > 0 ? this.TabCur - 1 : 0;
+      //执行切换页面的方法
+      // console.log("向右滑动");
+      // wx.navigateTo({
+      //   url: '../right/right'
+      // })
+    }
+    // 向右滑动   
+    if (touchMove - touchDot >= 40 && time < 10 && flag_hd == true) {
+      flag_hd = false;
+      this.TabCur = this.TabCur < 2 ? this.TabCur + 1 : 2;
+      //执行切换页面的方法
+      // console.log("向左滑动");
+      // wx.navigateTo({
+      //   url: '../left/left'
+      // })
+    }
+    clearInterval(interval); // 清除setInterval
+    time = 0;
   },
 
   /**
